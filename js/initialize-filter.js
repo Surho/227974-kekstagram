4d1563af8a1ea601b500ae58fae24d9f2e5b189a
Surho <surho007@hotmail.com>
2017-04-15 23:05:36 +0300

    initialize-

---
+++ js/initialize-filter.js
@@ -0,0 +1,71 @@
+'use strict';
+window.initializeFilters =  (function() {
+  
+    var uploadPreview = document.querySelector('.upload-form-preview');
+    var filterScroller = document.querySelector('.upload-filter-level');
+    var filterHandler = document.querySelector('.upload-filter-level-pin');
+    var filterLineVal = document.querySelector('.upload-filter-level-val');
+    var filterLine = document.querySelector('.upload-filter-level-line');
+
+    function activate(filterName, applyFilter) {
+    var filtersInfo = {
+      chrome: {
+        scale: 1/450,
+        cssValue: 'grayscale' ,
+        division: ''
+      },
+      sepia: {
+        scale: 1/450,
+        cssValue:'sepia',
+        division: ''
+      },
+      marvin: {
+        scale: 100/450,
+        cssValue:'invert',
+        division: '%'
+      },
+      phobos: {
+        scale: 3/450,
+        cssValue:'blur',
+        division: 'px'
+      },
+      heat: {
+        scale: 3/450,
+        cssValue: 'brightness',
+        division: ''
+      }
+    }
+
+    var filterObject = filtersInfo[filterName];
+    initializeFilters.activeFilter = filterName;
+    applyFilter(filterObject);
+  }
+
+  function hideScroller() {
+    filterScroller.style.display = 'none';
+  };
+
+  function showScroller() {
+    filterScroller.style.display = '';
+  };
+
+  function resetAllSettings() {
+    var resizeValue = document.querySelector('.upload-resize-controls-value');
+    initializeFilters.filterHandler.style.left = 90 + 'px';
+    initializeFilters.filterLineVal.style.width =  20 +'%';
+    resizeValue.setAttribute('value', 100 + '%');
+    uploadPreview.style.transform = 'scale(' + 1 + ')';
+    uploadPreview.style = '';
+  };
+
+  return {
+    activateFilter: activate,
+    resetAllSettings: resetAllSettings,
+    hideScroller: hideScroller,
+    showScroller: showScroller,
+    filterScroller: filterScroller,
+    filterHandler: filterHandler,
+    filterLineVal: filterLineVal,
+    filterLine: filterLine
+  };
+})();
---
+++ js/initialize-scale.js
@@ -0,0 +1,39 @@
+'use strict';
+window.initializeScale = function(scaleElement, adjustScale) {
+
+  var resizeControls = scaleElement;
+  var resizeDec = resizeControls.querySelector('.upload-resize-controls-button-dec');
+  var resizeInc = resizeControls.querySelector('.upload-resize-controls-button-inc');
+  var resizeValue = resizeControls.querySelector('.upload-resize-controls-value');
+
+  function resize(step, max, bigger) {
+    var currentValue = parseInt(resizeValue.value, 10);
+    function resizeBigger() {
+      if (currentValue < max) {
+        currentValue += step;
+        adjustScale(currentValue);
+        resizeValue.setAttribute('value', currentValue + '%');
+      }
+    }
+    function resizeSmaller() {
+      if (currentValue > step) {
+        currentValue -= step;
+        adjustScale(currentValue);
+        resizeValue.setAttribute('value', currentValue + '%');
+      }
+    }
+    (bigger) ? resizeBigger() : resizeSmaller();
+  }
+
+  scaleElement.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var target = evt.target;
+      if (evt.target === resizeDec) {
+         resize(25, 100); 
+      }
+      if (evt.target === resizeInc) {
+         resize(25, 100, true);
+      }
+      target=target.parentNode;
+  })
+}
